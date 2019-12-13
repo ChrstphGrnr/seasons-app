0e8439365b6e5152bc69585f420ca323abe93b7f
chrstphgrnr <christophgruener@me.com>
2019-12-13 10:46:36 -0700

    initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SeasonDisplay = () => {
+    return (
+        <div>Season Display!</div>
+    )
+}
+
+ReactDOM.render(<SeasonDisplay />, document.querySelector('#root'))
