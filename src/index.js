66455a27e306561c78e6f904bfb199c04064f38b
chrstphgrnr <christophgruener@me.com>
2019-12-16 09:51:04 -0700

    add conditional loading to the <App /> component

--- src/index.js
+++ src/index.js
@@ -5,22 +5,33 @@ class App extends React.Component {
     constructor(props) {        
         super(props);
         // debugger
-        this.state = { lat: null };
+        this.state = { lat: null , errorMessage: ""};
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude })
             }, 
-            (err)  => console.log(err) 
+            (err)  => this.setState(
+                {
+                    errorMessage: err.message
+                }) 
         );  
     }
     
     
     render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!...</div>
+
+
         
-        return (
-            <div>Latitude: {this.state.lat}</div>
-        )
     }
 }
 
